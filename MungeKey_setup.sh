87663976f6cd85b0c7ac819622a89aee9c92c7f3
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-04 18:12:39 -0500

    Create MungeKey_setup.sh

---
+++ MungeKey_setup.sh
@@ -0,0 +1,8 @@
+sudo /usr/sbin/create-munge-key -r
+sudo chmod 777 /etc/munge/munge.key
+sudo su root -c 'dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key'
+sudo chmod 700 /etc/munge/munge.key
+sudo chown munge: /etc/munge/munge.key
+sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.4:/etc/munge"
+sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.5:/etc/munge"
+sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.6:/etc/munge"
