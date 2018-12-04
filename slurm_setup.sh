f42f0c10d5ceaadc3adcc1e75ae8755522777bef
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-04 18:04:43 -0500

    Update slurm_setup.sh
    Removed munge key commands.

--- slurm_setup.sh
+++ slurm_setup.sh
@@ -8,11 +8,3 @@ sudo useradd -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g s
 sudo yum install epel-release
 sudo yum install munge munge-libs munge-devel -y
 sudo yum install rng-tools -y
-sudo /usr/sbin/create-munge-key -r
-sudo chmod 777 /etc/munge/munge.key
-sudo su root -c 'dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key'
-sudo chmod 400 /etc/munge/munge.key
-sudo chown munge: /etc/munge/munge.key
-sudo scp /etc/munge/munge.key 192.168.1.4:/etc/munge
-sudo scp /etc/munge/munge.key 192.168.1.5:/etc/munge
-sudo scp /etc/munge/munge.key 192.168.1.6:/etc/munge
