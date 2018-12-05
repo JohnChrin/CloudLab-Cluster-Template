215ac6bacde62a1dd45b58360f5d4982e937699f
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-05 15:50:03 -0500

    Update MungeKey_Create.sh
    Added commands to correct the owner of munge.key.

--- MungeKey_Create.sh
+++ MungeKey_Create.sh
@@ -4,5 +4,8 @@ sudo su root -c 'dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key'
 sudo chmod 700 /etc/munge/munge.key
 sudo chown munge: /etc/munge/munge.key
 sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.4:/etc/munge"
+ssh 192.168.1.4 sudo chown 991 /etc/munge/mung.key
 sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.5:/etc/munge"
+ssh 192.168.1.5 sudo chown 991 /etc/munge/mung.key
 sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.6:/etc/munge"
+ssh 192.168.1.6 sudo chown 991 /etc/munge/mung.key
