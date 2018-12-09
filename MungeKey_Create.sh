d1216265c916f1e103c249f5691894e086d026ab
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-08 21:06:55 -0500

    Update MungeKey_Create.sh
    Added some commands for head.

--- MungeKey_Create.sh
+++ MungeKey_Create.sh
@@ -1,3 +1,4 @@
+#THIS IS FOR THE HEAD NODE ONLY
 sudo /usr/sbin/create-munge-key -r
 sudo chmod 777 /etc/munge/munge.key
 sudo su root -c 'dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key'
@@ -9,3 +10,9 @@ sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.5:/etc/munge"
 ssh 192.168.1.5 sudo chown 991 /etc/munge/mung.key
 sudo su BW840606 -c "sudo scp /etc/munge/munge.key 192.168.1.6:/etc/munge"
 ssh 192.168.1.6 sudo chown 991 /etc/munge/mung.key
+sudo ssh 192.168.1.4 sudo systemctl enable munge
+sudo ssh 192.168.1.4 sudo systemctl start munge
+sudo ssh 192.168.1.5 sudo systemctl enable munge
+sudo ssh 192.168.1.5 sudo systemctl start munge
+sudo ssh 192.168.1.6 sudo systemctl enable munge
+sudo ssh 192.168.1.6 sudo systemctl start munge
