6d15b651d0f651e47e0ee6ba9b80f4371612810b
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-09 11:32:24 -0500

    Update profile.py
    Added new files to be run.

--- profile.py
+++ profile.py
@@ -40,6 +40,7 @@ for i in range( params.n ):
     node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'cp /local/repository/source/* /scratch/'"))
     node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'bash /local/repository/slurm_setup.sh'"))
     node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'bash /local/repository/MungeKey_Create.sh'"))
+    node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'bash /local/repository/Extra_Head_Slurm.sh'"))
     node.routable_control_ip = "true"
   elif i == 1:
     node = request.XenVM("metadata")
@@ -72,7 +73,7 @@ for i in range( params.n ):
     node.addService(pg.Execute(shell="sh", command="sleep 5m && sudo -H -u BW840606 bash -c '/local/repository/ssh_setup.sh'"))
     #node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'cp /local/repository/source/* /users/BW840606'"))
     node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'bash /local/repository/slurm_setup.sh'"))
-    #node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c '/local/repository/MungeKey_Setup.sh'"))
+    node.addService(pg.Execute(shell="sh", command="sudo su BW840606 -c 'bash /local/repository/Extra_Node_Slurm.sh'"))
     node.cores = 2
     node.ram = 4096
     
