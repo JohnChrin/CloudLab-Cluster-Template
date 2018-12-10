603a564e2f939453cb5cf1a499a4a498ce3ffb90
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-10 15:58:14 -0500

    Update MungeKey_Create.sh

--- MungeKey_Create.sh
+++ MungeKey_Create.sh
@@ -5,12 +5,16 @@ sudo su root -c 'dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key'
 sudo chmod 700 /etc/munge/munge.key
 sudo chown munge: /etc/munge/munge.key
 sudo cp /etc/munge/munge.key /scratch/munge.key
+ssh 192.168.1.2 sudo cp /scratch/munge.key /etc/munge/munge.key
+ssh 192.168.1.2 sudo chown 991 /etc/munge/munge.key
 ssh 192.168.1.4 sudo cp /scratch/munge.key /etc/munge/munge.key
 ssh 192.168.1.4 sudo chown 991 /etc/munge/munge.key
 ssh 192.168.1.5 sudo cp /scratch/munge.key /etc/munge/munge.key
 ssh 192.168.1.5 sudo chown 991 /etc/munge/munge.key
 ssh 192.168.1.6 sudo cp /scratch/munge.key /etc/munge/munge.key
 ssh 192.168.1.6 sudo chown 991 /etc/munge/munge.key
+ssh 192.168.1.2 sudo systemctl enable munge
+ssh 192.168.1.2 sudo systemctl start munge
 ssh 192.168.1.4 sudo systemctl enable munge
 ssh 192.168.1.4 sudo systemctl start munge
 ssh 192.168.1.5 sudo systemctl enable munge
