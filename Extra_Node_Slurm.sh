cdc31c428fed2820cd5e7e4997f342c12ed34dab
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-11 21:02:08 -0500

    Update Extra_Node_Slurm.sh
    Fixed archive with new local archive.

--- Extra_Node_Slurm.sh
+++ Extra_Node_Slurm.sh
@@ -1,8 +1,8 @@
 cd ~ && sudo yum install rpm-build gcc openssl openssl-devel libssh2-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel gtk2-devel man2html libibmad libibumad perl-Switch perl-ExtUtils-MakeMaker -y
 cd ~ && sudo mkdir slurm
-cd ~/slurm && sudo wget http://www.schedmd.com/download/latest/slurm-18.08.3.tar.bz2
+#cd ~/slurm && sudo wget http://www.schedmd.com/download/latest/slurm-18.08.4.tar.bz2
 cd ~/slurm && sudo yum install rpm-build
-cd ~/slurm && sudo rpmbuild -ta slurm-18.08.3.tar.bz2
+cd ~/slurm && sudo rpmbuild -ta /local/repository/slurm-18.08.4.tar.bz2
 sudo mkdir /scratch/slurm-rpms
 sudo cp /root/rpmbuild/RPMS/x86_64/* /scratch/slurm-rpms
 sudo yum --nogpgcheck localinstall /scratch/slurm-rpms/* -y
