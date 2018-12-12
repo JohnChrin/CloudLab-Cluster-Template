212a8c6cc70110d5f5a58c24383c2964d163941b
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-12 11:49:32 -0500

    Update Extra_Head_Slurm.sh
    Changed to work with more nodes.

--- Extra_Head_Slurm.sh
+++ Extra_Head_Slurm.sh
@@ -1,8 +1,9 @@
-cd ~ && sudo yum install rpm-build gcc openssl openssl-devel libssh2-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel gtk2-devel man2html libibmad libibumad perl-Switch perl-ExtUtils-MakeMaker -y
-cd ~ && sudo mkdir slurm
+sudo yum install rpm-build gcc openssl openssl-devel libssh2-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel gtk2-devel man2html libibmad libibumad perl-Switch perl-ExtUtils-MakeMaker -y
+#cd ~ && sudo mkdir slurm
 #cd ~/slurm && sudo wget http://www.schedmd.com/download/latest/slurm-18.08.4.tar.bz2
 cd ~/slurm && sudo yum install rpm-build
-cd ~/slurm && sudo rpmbuild -ta /local/repository/slurm-18.08.4.tar.bz2
+#cd ~/slurm
+sudo rpmbuild -ta /local/repository/slurm-18.08.4.tar.bz2
 sudo mkdir /scratch/slurm-rpms
 sudo cp /root/rpmbuild/RPMS/x86_64/* /scratch/slurm-rpms
 sudo touch /scratch/rpm.fin
@@ -30,5 +31,26 @@ sudo yum install ntp -y
 sudo chkconfig ntpd on
 sudo ntpdate pool.ntp.org
 sudo systemctl start ntpd
+#enable slurm daemon
+while [ ! -f /scratch/d.fin ]
+do
+  sleep 5
+done
 sudo systemctl enable slurmctld.service
 sudo systemctl start slurmctld.service
+#notify metadata slurmctld daemon is up
+sudo touch /scratch/head.fin
+
+##wait for callback from metadata
+#while [ ! -f /scratch/cluster.fin ]
+#do
+#  sleep 5
+#done
+#
+##restart slurmctld daemon
+#sudo systemctl restart slurmctld
+#
+##remove setup files
+#cd /scratch
+#sudo rm d.fin dbd.fin dbd.sql innodb.cnf metakey.fin munge.key rpm.fin setup.sql slurm.conf slurmdbd.conf
+
