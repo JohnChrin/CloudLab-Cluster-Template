a0b0ddbc06331d9dc3f21f0b7e1a8db102a9d40c
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-12 11:42:48 -0500

    Update MungeKey_Create.sh

--- MungeKey_Create.sh
+++ MungeKey_Create.sh
@@ -1,3 +1,14 @@
+sudo yum install mariadb-server mariadb-devel -y
+export MUNGEUSER=991
+sudo groupadd -g $MUNGEUSER munge
+sudo useradd -m -c "Munge Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge -s /sbin/nologin munge
+export SLURMUSER=992
+sudo groupadd -g $SLURMUSER slurm
+sudo useradd -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm -s /bin/bash slurm
+sudo yum install epel-release -y
+sudo yum install munge munge-libs munge-devel -y
+sudo yum install rng-tools -y
+sudo mkdir /etc/munge/
 #THIS IS FOR THE HEAD NODE ONLY
 #WARNING: the command below may hang, useage of -r has caused this previously.
 sudo /usr/sbin/create-munge-key -f
@@ -6,6 +17,14 @@ sudo su root -c 'dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key'
 sudo chmod 700 /etc/munge/munge.key
 sudo chown munge: /etc/munge/munge.key
 sudo cp /etc/munge/munge.key /scratch/munge.key
+#correct permissions
+sudo chown -R munge: /etc/munge/ /var/log/munge/
+sudo chmod 0700 /etc/munge/ /var/log/munge/
+while [ ! -f /scratch/metakey.fin ]
+do
+  sleep 10
+done
+
 #ssh 192.168.1.2 sudo mkdir /etc/munge/
 #ssh 192.168.1.2 sudo cp /scratch/munge.key /etc/munge/munge.key
 #ssh 192.168.1.2 sudo chown 991 /etc/munge/munge.key
