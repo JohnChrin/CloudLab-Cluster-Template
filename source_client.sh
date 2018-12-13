2971eeb7b38f143d6392ea63b7d4de7623fb33e3
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-13 16:21:59 -0500

    Update source_client.sh
    Added command to source mpi system-wide.

--- source_client.sh
+++ source_client.sh
@@ -1,3 +1,4 @@
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/BW840606/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/BW840606/.bashrc
 source /users/BW840606/.bashrc
+sudo su root -c 'echo 'PATH="/software/openmpi/3.1.2/bin:$PATH"' >> /etc/environment'
